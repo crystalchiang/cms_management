c2d3d3bf286be5b12b4a6851538c0e9a41bfb651
crystalchiang <tophet0929@gmail.com>
2021-05-18 04:02:06 +0800

    Update 2019_12_10_092113_create_permission_tables.php

--- database/migrations/2019_12_10_092113_create_permission_tables.php
+++ database/migrations/2019_12_10_092113_create_permission_tables.php
@@ -16,12 +16,12 @@ class CreatePermissionTables extends Migration
         $tableNames = config('permission.table_names');
         $columnNames = config('permission.column_names');
 
-        Schema::create($tableNames['permissions'], function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('guard_name');
-            $table->timestamps();
-        });
+//         Schema::create($tableNames['permissions'], function (Blueprint $table) {
+//             $table->bigIncrements('id');
+//             $table->string('name');
+//             $table->string('guard_name');
+//             $table->timestamps();
+//         });
 
 //         Schema::create($tableNames['roles'], function (Blueprint $table) {
 //             $table->bigIncrements('id');
@@ -97,6 +97,6 @@ class CreatePermissionTables extends Migration
         Schema::drop($tableNames['model_has_roles']);
         Schema::drop($tableNames['model_has_permissions']);
 //         Schema::drop($tableNames['roles']);
-        Schema::drop($tableNames['permissions']);
+//         Schema::drop($tableNames['permissions']);
     }
 }
